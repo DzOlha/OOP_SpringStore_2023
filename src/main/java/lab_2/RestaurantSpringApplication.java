9f1af238f44ecebb10c439ffd0fe90d6a2e1fa2e
DzOlha <monsarda11@gmail.com>
2023-06-11 21:11:03 +0300

    Created the project, configured the database and all dependencies.

---
+++ src/main/java/lab_2/RestaurantSpringApplication.java
@@ -0,0 +1,12 @@
+package lab_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantSpringApplication.class, args);
+    }
+
+}
